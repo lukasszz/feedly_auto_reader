744ad7d589787867f9c727dd02639357c92ee9f8
lukasz <lukasz>
2016-03-31 21:11:02 +0200

    Create run file - imports

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 import configparser
 
-from FeedlyClient.client import FeedlyClient
-from feedly_auto_reader.reader import _logger_setup, get_unread_feeds, get_unread_entries, mark_entries_read
+from FeedlyClient import client
+from feedly_auto_reader import reader
 
-_logger_setup()
+reader._logger_setup()
 ini = configparser.ConfigParser()
 ini.read('config.ini')
-fclient = FeedlyClient(sandbox=False, token=ini['FEEDLY_USER']['token'])
-feeds = get_unread_feeds(fclient)
-old_entries = get_unread_entries(fclient, feeds, int(ini['AUTO_READER']['entries_older_than']))
-mark_entries_read(old_entries, fclient)
+fclient = client.FeedlyClient(sandbox=False, token=ini['FEEDLY_USER']['token'])
+feeds = reader.get_unread_feeds(fclient)
+old_entries = reader.get_unread_entries(fclient, feeds, int(ini['AUTO_READER']['entries_older_than']))
+reader.mark_entries_read(old_entries, fclient)
